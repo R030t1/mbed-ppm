91e99034ec6286216327443bdc02e4a653d1103d
Sid Spry <github@sid.aeam.us>
2021-01-31 21:53:44 -0600

    Working skeleton project

---
+++ setup.sh
@@ -0,0 +1,1 @@
+pip install mbed-cli pyyaml jsonschema mbed_cloud_sdk jinja2 mbed_ls mbed_host_tests mbed_greentea pyelftools manifest_tool icetea pycryptodome pyusb cmsis_pack_manager psutil cryptography click
