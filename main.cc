b567f9c1b40a1bf385342c93e2e2c4f8b9344a1d
Sid Spry <github@sid.aeam.us>
2021-02-02 00:13:19 -0600

    Add base class for output

--- main.cc
+++ main.cc
@@ -2,7 +2,29 @@
 #include "rtos.h"
 #include <string.h>
 
-int main() {
+class PPMOut {
+public:
+	PPMOut();
+protected:
+	Thread worker;
+	void work();
+};
+
+PPMOut::PPMOut() {
+	worker.start(callback(this, &PPMOut::work));
+}
+
+void PPMOut::work() {
 	while (true) {
+		ThisThread::sleep_for(100);
+	}
+}
+
+int main() {
+	PPMOut pout();
+
+	while (true) {
+		// Why no units? Important, figure out what changed. (e.g. 100ms)
+		ThisThread::sleep_for(100);
 	}
 }
