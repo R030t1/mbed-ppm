fdf4830e1cda39631c4bb2bcb275ec5a24017615
Sid Spry <github@sid.aeam.us>
2021-03-08 12:50:04 -0600

    PPM output should work, test board fails to respond

--- main.cc
+++ main.cc
@@ -2,29 +2,50 @@
 #include "rtos.h"
 #include <string.h>
 
+#include <chrono>
+using namespace std::chrono;
+
+DigitalOut led(LED1);
+DigitalOut ppm(D2);
+
 class PPMOut {
 public:
-	PPMOut();
+	PPMOut(DigitalOut pin);
 protected:
-	Thread worker;
-	void work();
+	Timeout timeout;
+	DigitalOut _pin;
+	bool state;
+
+	void timer_expired();
 };
 
-PPMOut::PPMOut() {
-	worker.start(callback(this, &PPMOut::work));
+PPMOut::PPMOut(DigitalOut pin) : _pin(pin) {
+	state = 0;
+	timeout.attach_us(callback(this, &PPMOut::timer_expired), 1);
 }
 
-void PPMOut::work() {
-	while (true) {
-		ThisThread::sleep_for(100);
+void PPMOut::timer_expired() {
+	if (!state) {
+		// Off to on.
+		state = 1; _pin = 1;
+		timeout.attach_us(callback(this, &PPMOut::timer_expired), 1400);
+		led = !led;
+	} else if (state) {
+		// On to off.
+		state = 0; _pin = 0;
+		timeout.attach_us(callback(this, &PPMOut::timer_expired), 2000 - 1400);
+		led = !led;
 	}
 }
 
 int main() {
-	PPMOut pout();
+	led = 0;
+	printf("=== boot.\r\n");
+	PPMOut pout(ppm);
 
 	while (true) {
 		// Why no units? Important, figure out what changed. (e.g. 100ms)
-		ThisThread::sleep_for(100);
+		ThisThread::sleep_for(1000);
+		printf(".\r\n");
 	}
 }
